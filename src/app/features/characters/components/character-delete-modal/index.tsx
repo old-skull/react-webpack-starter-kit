9727a0131eea47980609adce53aed6aee9687c50
Alex <tictak211@gmail.com>
2022-01-16 20:42:05 +0200

    test: update character-page tests

--- src/app/features/characters/components/character-delete-modal/index.tsx
+++ src/app/features/characters/components/character-delete-modal/index.tsx
@@ -1,5 +1,4 @@
 import {
-  Box,
   Button,
   Code,
   Modal,
@@ -21,28 +20,26 @@ interface ICharacterDeleteModal {
 
 export const CharacterDeleteModal: FC<ICharacterDeleteModal> = ({ onConfirm, onClose, isOpen }) => {
   return (
-    <Box className="character-delete-modal-container">
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Confirm action</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody display="grid" gridGap={4}>
-            <Text>Are you sure you want to delete character?</Text>
-            <Text fontStyle="italic">
-              You will be navigated to the <Code>/characters</Code>
-            </Text>
-          </ModalBody>
-          <ModalFooter display="flex" justifyContent="space-between">
-            <Button colorScheme="yellow" mr={3} onClick={onClose}>
-              No
-            </Button>
-            <Button colorScheme="teal" onClick={onConfirm}>
-              Yes
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-    </Box>
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Confirm action</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody display="grid" gridGap={4}>
+          <Text>Are you sure you want to delete character?</Text>
+          <Text fontStyle="italic">
+            You will be navigated to the <Code>/characters</Code>
+          </Text>
+        </ModalBody>
+        <ModalFooter display="flex" justifyContent="space-between">
+          <Button colorScheme="yellow" mr={3} onClick={onClose}>
+            No
+          </Button>
+          <Button colorScheme="teal" onClick={onConfirm}>
+            Yes
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
   );
 };
